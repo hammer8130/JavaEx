d04ff3c447f7dd1bbce3d0cd37a029a16f920c97
정우찬 <hammeri@naver.com>
2024-04-11 16:22:48 +0900

    4/11 fileiostream 까지

---
+++ src/com/javaex/io/bytestream/ByteArrayStreamEx.java
@@ -0,0 +1,94 @@
+package com.javaex.io.bytestream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+public class ByteArrayStreamEx {
+
+	public static void main(String[] args) {
+//		입력 소스
+		byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};	
+//		출력 타겟
+		byte[] outSrc = null;
+		System.out.println("입력 소스: "+Arrays.toString(inSrc));
+		
+		// 스트림들 열기
+//		전통적 방식의 입출력 스트림 예외 처리
+//		Autoclosable -> 자원 자동 해제
+//		InputStream ip = null;
+//		OutputStream op = null;
+//		
+//		
+////		try - with - resource
+//		// 이곳은 main method라서 throw를 해도 처리할 수가 없다. 따라서 try/catch로 묶을것.
+//		try {
+//		ip = new ByteArrayInputStream(inSrc);
+//		op = new ByteArrayOutputStream();
+//		
+//		int data =0; // 입력 스트림으로부터 넘어올 데이터 저장 변수
+//		
+//		
+//				while((data=ip.read())!=-1) {
+//				System.out.println("Read data:"+data);
+//				op.write(data); // 잠재적으로 io exception 발생 가능
+//				}
+//				
+//			outSrc = ((ByteArrayOutputStream)op).toByteArray();
+//			System.out.println("최종 결과: "+Arrays.toString(outSrc));
+//				
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			} catch (Exception e) { // 혹시 모를 예외 처리
+//				e.printStackTrace();
+//			} finally {
+////				if(ip !=null)
+////					try {
+////						ip.close();
+////					} catch (IOException e) {
+////						e.printStackTrace();
+////					}
+////				if(op !=null)
+////					try {
+////						op.close();
+////					} catch (IOException e) {
+////						e.printStackTrace();
+////					}
+//				try {
+//					
+//				ip.close();
+//				op.close();
+//				}catch(IOException e) {
+//					
+//				}
+//			}
+		
+		// AutoCloseable을 활용한 예외 처리: try - with -resource
+		try (	// 자원 해제를 필요로 하는 객체를 초기화
+			InputStream is = new ByteArrayInputStream(inSrc);
+			OutputStream os = new ByteArrayOutputStream();
+				){
+			int data=0; // 입력 스트림으로부터 임력된 데이터 저장할 변수
+			
+			while((data=is.read())!=-1) {
+				System.out.println("Read data:"+ data);
+				os.write(data);
+			}
+			
+			outSrc = ((ByteArrayOutputStream)os).toByteArray();
+			System.out.println("최종 결과: "+Arrays.toString(outSrc));
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/com/javaex/io/bytestream/FileClassEx.java
@@ -0,0 +1,84 @@
+package com.javaex.io.bytestream;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileClassEx {
+	// files의 경로 위치 
+	private static String rootPath = System.getProperty("user.dir")+"\\files";
+	
+
+	public static void main(String[] args) {
+		  System.out.println("루트 경로:"+rootPath);
+		  File f = new File(rootPath);
+		  System.out.println(rootPath + ":"+(f.exists()?"존재":"없음"));
+		  
+		  // 정보 출력
+		  printFileInfo(f);
+		  
+		  // 디렉터리 만들기
+		  // files 디렉토리 아래 ==> files\\subdir\\subdir2 -> 디렉토리 생성
+		  File newDir = new File(rootPath + "\\subdir\\subdir2"); // 파일(디렉토리) 정보
+		  if(!newDir.exists()) { // 존재 여부 확인
+			  if(newDir.mkdirs()) { // 실제 생성
+				  System.out.println(newDir+"을 생성했습니다.");
+			  }
+		  }
+		  printFileInfo(f);
+		  
+		  // 새로 생성한 files//subdir 디렉토리에 새파일 myfile.txt를 만들어 보자.
+		  File newFile = new File(rootPath + "\\subdir\\myfile.txt");
+		  // 파일 객체 생성 -> 정보객체 생성하는 것, 파일을 만드는 것은 아니다
+		  if(!newFile.exists()) { // 존재 여부를 확인 후, 없으면 생성
+			  try {
+				newFile.createNewFile(); // 실제 파일 생성
+			} catch (IOException e) {
+				System.err.println("파일을 만들지 못했어요.");
+			} 
+		  }
+		  // 목록 출력
+		  printFileInfo(f);
+		  
+		  // 파일 삭제
+		  newFile.delete();
+	}
+	
+	private static void printFileInfo(File f) {
+		System.out.println("---------");
+		
+		// 전달받은 파일 객체가 디렉토리인가?
+		if(f.isDirectory()) { // 디렉토리면, 목록을 출력한다.
+			System.out.println("디렉토리:"+f.getName());
+			
+			// 파일 목록 가져오기
+			File[] files = f.listFiles(); // 파일 정보의 배열
+			
+			// 목록 정보 출력
+			/*
+			 * f rwx 파일명		파일사이즈
+			 * d rwx 디렉토리명
+			 */
+			
+			for(File fl : files) {
+				System.out.print(fl.isDirectory()?"d":"f"+" ");
+				System.out.print(fl.canRead()?"r":"."+" ");
+				System.out.print(fl.canWrite()?"w":"."+" ");
+				System.out.print(fl.canExecute()?"x":"."+" ");
+				System.out.print(fl.isHidden()?"h":"."+" ");
+				
+				System.out.print(" "+fl.getName());  // 파일명 표시
+				System.out.println("\t"+fl.length());
+			}
+		}else { // 파일 -> 간략정보
+			System.out.println("File:");
+			// 간략한 파일 정보
+			System.out.print(f.getName()+"\t"); // 파일명
+			System.out.print(f.canRead()?"r":".");
+			System.out.print(f.canWrite()?"w":"."); 
+			System.out.print(f.canExecute()?"x":"."); // 실행여부
+			System.out.print(f.isHidden()?"h":".");
+			System.out.println();
+		}
+		
+	}
+}
